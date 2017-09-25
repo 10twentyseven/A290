d4dfd5db971694887fba330ae77a50d8aac056a6
Kevin Boehle <kevinboehle@yahoo.com>
2017-09-25 12:32:24 -0400

    Create ToDoInput.java

---
+++ Assignment4/ToDoInput.java
@@ -0,0 +1,23 @@
+package com.a290.kevin.assignment5;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kevin on 9/25/2017.
+ */
+
+public class ToDoInput extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
