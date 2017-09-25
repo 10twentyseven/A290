6ed2a5d603c3e3e896490183934d6b7ea2dfdf0f
Kevin Boehle <kevinboehle@yahoo.com>
2017-09-25 12:32:54 -0400

    Create ToDo.java

---
+++ Assignment4/ToDo.java
@@ -0,0 +1,31 @@
+package com.a290.kevin.assignment5;
+
+/**
+ * Created by Kevin on 9/13/2017.
+ */
+
+public class ToDo {
+    private String title;
+    private String description;
+
+    public ToDo(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
