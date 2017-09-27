349a38940968ccf84edfe4c31b1a491bf8237df3
Kevin Boehle <kevinboehle@yahoo.com>
2017-09-27 09:08:47 -0400

    Create ToDoChange.java

---
+++ Assignment4/ToDoChange.java
@@ -0,0 +1,21 @@
+package com.a290.kevin.assignment4;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kevin on 9/25/2017.
+ */
+
+public class ToDoChange extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.todochange_layout, container, false);
+    }
+}
