8dc0935e6159346bc3100eb75aeebe1f6b414a80
Kevin Boehle <kevinboehle@yahoo.com>
2017-09-27 09:05:48 -0400

    Update MainActivity.java

--- Assignment4/MainActivity.java
+++ Assignment4/MainActivity.java
@@ -1,4 +1,4 @@
-package com.a290.kevin.assignment5;
+package com.a290.kevin.assignment4;
 
 import android.app.Fragment;
 import android.os.Bundle;
@@ -9,7 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,8 +23,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<ToDo> myToDoList = new ArrayList<ToDo>();
-    android.app.FragmentManager fmanager = getFragmentManager();
-    android.app.FragmentTransaction ftransaction = fmanager.beginTransaction();
+    int pos = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +33,93 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final android.app.FragmentManager fmanager = getFragmentManager();
+        final android.app.FragmentTransaction ftransaction = fmanager.beginTransaction();
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        ftransaction.commit();
+
+
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final android.app.FragmentManager fmanager = getFragmentManager();
+                final android.app.FragmentTransaction ftransaction = fmanager.beginTransaction();
                 Fragment toDoInput = new ToDoInput();
-                ftransaction.replace(R.id.myToDoListView, toDoInput);
+                ftransaction.replace(R.id.main_activity_layout, toDoInput);
+                ftransaction.commit();
+                ListView currentList = (ListView) findViewById(R.id.myToDoListView);
+                System.out.println(currentList.getVisibility());
+                currentList.setVisibility(View.INVISIBLE);
             }
         });
 
         populateListView();
-        populateList();
     }
 
     private void populateListView() {
         ArrayAdapter<ToDo> myAdapter = new myListAdapter();
-        ListView myListViewer = (ListView) findViewById(R.id.myToDoListView);
+        final ListView myListViewer = (ListView) findViewById(R.id.myToDoListView);
         myListViewer.setAdapter(myAdapter);
+        myListViewer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                final android.app.FragmentManager fmanager = getFragmentManager();
+                final android.app.FragmentTransaction ftransaction = fmanager.beginTransaction();
+                Fragment toDoChange = new ToDoChange();
+                ftransaction.replace(R.id.main_activity_layout, toDoChange);
+                ftransaction.commit();
+
+                pos = position;
+                ToDo currentToDo = myToDoList.get(position);
+
+
+                ListView currentList = (ListView) findViewById(R.id.myToDoListView);
+                currentList.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
-    private void populateList() {
-        myToDoList.add(new ToDo("Aerosmith", "Rock and Roll BAYBEEEEE"));
+    private void populateList(String title, String description) {
+        myToDoList.add(new ToDo(title, description));
+    }
+
+    public void saveButtonHandler(View view) {
+        TextView titleTextView = (TextView) findViewById(R.id.titleChange);
+        String title = titleTextView.getText().toString();
+        TextView descriptionTextView = (TextView) findViewById(R.id.contentsChange);
+        String description = descriptionTextView.getText().toString();
+        populateList(title, description);
+        ListView currentList = (ListView) findViewById(R.id.myToDoListView);
+        currentList.setVisibility(View.VISIBLE);
+        View input = (View) findViewById(R.id.toDoInputView);
+        input.setVisibility(View.GONE);
+    }
+
+    public void deletebuttonhandler(View view) {
+        myToDoList.remove(pos);
+        populateListView();
+        ListView currentList = (ListView) findViewById(R.id.myToDoListView);
+        currentList.setVisibility(View.VISIBLE);
+        View input = (View) findViewById(R.id.toDoInputView);
+        input.setVisibility(View.GONE);
+
+    }
+
+    public void editsaveButtonHandler(View view) {
+        myToDoList.remove(pos);
+        EditText titleTextView = (EditText) findViewById(R.id.titleEdit);
+        String title = titleTextView.getText().toString();
+        TextView descriptionTextView = (TextView) findViewById(R.id.contentsEdit);
+        String description = descriptionTextView.getText().toString();
+        myToDoList.add(new ToDo(title, description));
+        populateListView();
+        ListView currentList = (ListView) findViewById(R.id.myToDoListView);
+        currentList.setVisibility(View.VISIBLE);
+        View input = (View) findViewById(R.id.toDoInputView);
+        input.setVisibility(View.GONE);
     }
 
     private class myListAdapter extends ArrayAdapter<ToDo> {
